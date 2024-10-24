ee80e432d7cf542b5bc39c9df9779560f83a7743
omkarbandikatte <omkarbandikatte2602@gmail.com>
2024-10-25 00:36:25 +0530

    setup

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 setup(
 name='MachineLearningProject',
 version='0.0.1',
